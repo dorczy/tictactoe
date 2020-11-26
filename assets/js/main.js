9443756933c0a844df6201bee768e62ddeca9af5
Kovács Dorottya <kovacsdorika@gmail.com>
2020-11-26 16:19:28 +0100

    Final.

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,44 +4,15 @@ let matrix;
 let stepCount = 0;
 let mark = 'X';
 
+
+const startGame = () => {
+    initState();
+    addClickListener();
+    newGame();
+}
+
 const initState = () => {
     matrix = Array(cols).fill(null).map(() => Array(rows).fill(null))
-}
-
-const changeMatrixValue = (element) => {
-    const row = parseInt(element.dataset.row, 10);
-    const cell = parseInt(element.dataset.cell, 10);
-    matrix[row][cell] = element.textContent;
-}
-
-const deleteSigns = () => {
-    document
-        .querySelectorAll('.tictactoe__cell')
-        .forEach(element => {
-            element.textContent = '';
-        });
-}
-
-
-const increaseCounter = () => {
-    stepCount += 1;
-}
-
-const modifyCell = (element) => {
-    element.textContent = mark;
-    element.removeEventListener('click', handleClick);
-}
-
-const setMark = () => {
-    mark = mark === 'X' ? 'O' : 'X';
-}
-
-const handleClick = (event) => {
-    increaseCounter();
-    modifyCell(event.target);
-    setMark();
-    changeMatrixValue(event.target);
-    checkWinner();
 }
 
 const addClickListener = () => {
@@ -52,50 +23,67 @@ const addClickListener = () => {
         });
 }
 
-const removeAllClickListener = () => {
-    document.querySelectorAll('.tictactoe__cell')
-        .forEach(element => {
-            element.removeEventListener('click', handleClick)
-        });
-}
-
-const checkValues = (arr) => arr.map(row =>
-    row.every((value) => value === 'X') ||
-    row.every((value) => value === 'O'))
-    .indexOf(true) !== -1;
-
-const checkColumnValues = () =>
-    checkValues(matrix.map((arr, i) => arr.map((item, j) => matrix[j][i])))
-
-const checkDiagonalValues = () =>
-    checkValues([
-        matrix.map((arr, i) => matrix[i][i]),
-        matrix.map((arr, i) => matrix[i][matrix[i].length - i - 1])
-    ])
-
-const checkWinner = () => {
-    console.log(checkColumnValues(), checkDiagonalValues());
-    if (checkValues(matrix) || checkColumnValues() || checkDiagonalValues()) {
-        endGame();
+    const handleClick = (event) => {
+        increaseCounter();
+        modifyCell(event.target);
+        setMark();
+        changeMatrixValue(event.target);
+        checkWinner();
     }
-}
 
-const setMessage = (message) => {
-    document
-        .querySelector('.winnerOutput')
-        .textContent = message
-}
+        const increaseCounter = () => {
+            stepCount += 1;
+        }
 
-const startGame = () => {
-    initState();
-    addClickListener();
-    newGame();
-}
+        const modifyCell = (element) => {
+            element.textContent = mark;
+            element.removeEventListener('click', handleClick);
+        }
 
-const endGame = () => {
-    setMessage('Nyertes: ' + (mark === 'X' ? 'O' : 'X'));
-    removeAllClickListener();
-}
+        const setMark = () => {
+            mark = mark === 'X' ? 'O' : 'X';
+        }
+
+        const changeMatrixValue = (element) => {
+            const row = parseInt(element.dataset.row, 10);
+            const cell = parseInt(element.dataset.cell, 10);
+            matrix[row][cell] = element.textContent;
+        }
+
+        const checkWinner = () => {
+            console.log(checkColumnValues(), checkDiagonalValues());
+            if (checkValues(matrix) || checkColumnValues() || checkDiagonalValues()) {
+                endGame();
+            }
+        }
+
+            const checkValues = (arr) => arr.map(row =>
+                row.every((value) => value === 'X') ||
+                row.every((value) => value === 'O'))
+                .indexOf(true) !== -1;
+
+            const checkColumnValues = () =>
+                checkValues(matrix.map((arr, i) => arr.map((item, j) => matrix[j][i])))
+
+            const checkDiagonalValues = () =>
+                checkValues([
+                    matrix.map((arr, i) => matrix[i][i]),
+                    matrix.map((arr, i) => matrix[i][matrix[i].length - i - 1])
+                ])
+
+            const endGame = () => {
+                setMessage('Nyertes: ' + (mark === 'X' ? 'O' : 'X'));
+                removeAllClickListener();
+            }
+
+                const removeAllClickListener = () => {
+                    document.querySelectorAll('.tictactoe__cell')
+                        .forEach(element => {
+                            element.removeEventListener('click', handleClick)
+                        });
+                }
+            
+
 
 const newGame = () => {
     document
@@ -108,5 +96,96 @@ const newGame = () => {
             setMark()
         })
 }
+
+/*  const initState = () => {
+        matrix = Array(cols).fill(null).map(() => Array(rows).fill(null))
+    } */
+
+/*  const addClickListener = () => {
+        document
+            .querySelectorAll('.tictactoe__cell')
+            .forEach(element => {
+                element.addEventListener('click', handleClick)
+            });
+    }
+
+        const handleClick = (event) => {
+            increaseCounter();
+            modifyCell(event.target);
+            setMark();
+            changeMatrixValue(event.target);
+            checkWinner();
+        }
+
+            const increaseCounter = () => {
+                stepCount += 1;
+            }
+
+            const modifyCell = (element) => {
+                element.textContent = mark;
+                element.removeEventListener('click', handleClick);
+            }
+
+            const setMark = () => {
+                mark = mark === 'X' ? 'O' : 'X';
+            }
+
+            const changeMatrixValue = (element) => {
+                const row = parseInt(element.dataset.row, 10);
+                const cell = parseInt(element.dataset.cell, 10);
+                matrix[row][cell] = element.textContent;
+            }
+
+            const checkWinner = () => {
+                console.log(checkColumnValues(), checkDiagonalValues());
+                if (checkValues(matrix) || checkColumnValues() || checkDiagonalValues()) {
+                    endGame();
+                }
+            }
+
+                const checkValues = (arr) => arr.map(row =>
+                    row.every((value) => value === 'X') ||
+                    row.every((value) => value === 'O'))
+                    .indexOf(true) !== -1;
+
+                const checkColumnValues = () =>
+                    checkValues(matrix.map((arr, i) => arr.map((item, j) => matrix[j][i])))
+
+                const checkDiagonalValues = () =>
+                    checkValues([
+                        matrix.map((arr, i) => matrix[i][i]),
+                        matrix.map((arr, i) => matrix[i][matrix[i].length - i - 1])
+                    ]) 
+
+                const endGame = () => {
+                setMessage('Nyertes: ' + (mark === 'X' ? 'O' : 'X'));
+                removeAllClickListener();
+            }
+
+                    const removeAllClickListener = () => {
+                        document.querySelectorAll('.tictactoe__cell')
+                            .forEach(element => {
+                                element.removeEventListener('click', handleClick)
+                            });
+                }*/
+
+    const deleteSigns = () => {
+        document
+            .querySelectorAll('.tictactoe__cell')
+            .forEach(element => {
+                element.textContent = ' ';
+            });
+}
+
+    const setMessage = (message) => {
+        document
+            .querySelector('.winnerOutput')
+            .textContent = message
+    }
+
+/*  const setMark = () => {
+        mark = mark === 'X' ? 'O' : 'X';
+    } */
+
 
 startGame();
